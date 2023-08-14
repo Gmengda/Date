24393ef3eb92f7fb1a328a1e09dc21d80e18f617
puffer <810833169@qq.com>
2023-08-14 11:57:15 +0800

    2023.8.14

---
+++ 长度为3且个字符不同的字符串.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+        if(s.size() < 3)
+        {
+            return 0;
+        }
+        int num = 0;
+        for(int i = 0 ; i < s.size() - 2 ; ++i)
+        {
+            if(s[i] != s[i + 1] && s[i] != s[i + 2] && s[i + 1] != s[i + 2])
+            {
+                ++num;
+            }
+        }
+        return num;
+    }
+};
